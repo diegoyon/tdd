05e7010f3513b76d6c73c464002f9753255f0e0b
Diego Yon <diegoyon@outlook.com>
2022-10-06 09:31:22 -0600

    created 3 pending tests

---
+++ spec/solver_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../solver.rb'
+
+describe Solver do
+  describe '#factorial' do
+    it 'returns the factorial of n'
+  end
+
+  describe '#reverse' do
+    it 'returns the word reversed'
+  end
+
+  describe '#fizzbuzz' do
+    it 'takes an integer n and returns a string'
+  end
+end
